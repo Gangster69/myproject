c5c646bd438c9632de2faf4f04ccc11b776ce975
user <user@es302n18>
2018-10-09 12:02:39 +0800

    sec

--- myproject/src/com/tom/hello.java
+++ myproject/src/com/tom/hello.java
@@ -4,13 +4,23 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age=20;
+		Person p =new Person(65.5f, 1.7f);
+//		p.weight = 65.5f;
+//		p.height = 1.7f;
+		System.out.println(p.bmi());
+		p.hello();
+//		Person hank = new Person();
+//		Person jack = null;
+//		jack.hello();
+		
+	    int age=20;
 		age=87;
 		Short day=12;
 		age=day;
 		float weight=35.45f;
 		float num=(float)65.8;
 		System.out.println("age");
+		
 	}
 
 }
