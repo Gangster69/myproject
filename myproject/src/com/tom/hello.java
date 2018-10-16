50f2779385d16ab29449c8d2b32c6005a94e90e0
user <user@es302n31>
2018-10-16 11:59:40 +0800

    20181016

--- myproject/src/com/tom/hello.java
+++ myproject/src/com/tom/hello.java
@@ -12,15 +12,35 @@ public class hello {
 //		Person hank = new Person();
 //		Person jack = null;
 //		jack.hello();
+//		Systwm.out.println(Hank 70 90  80 )
+//		String name ="Hank";
+//		int english =70;
+//		int math=90;
+//		System.out.println(name+ "\t" +english+ "\t" +math+"\t"+(english+math)/2);
 		
-	    int age=20;
-		age=87;
-		Short day=12;
-		age=day;
-		float weight=35.45f;
-		float num=(float)65.8;
-		System.out.println("age");
+		Student stu =new Student();
+		stu.name = "Hank";
+		stu.english = 70;
+		stu.math = 90;
+		stu.print();
 		
+		System .out.println(3 <= 5);
+		
+		String s1 = new String("abc");
+		String s2 = new String("abc");
+		System .out.println(s1.equals(s2));
+		
+		String s3="abc";
+		String s4="abc";
+		System.out.println(s3==s4);
+		
+	    // int age=20;
+		//age=87;
+		//Short day=12;
+		//age=day;
+		//float weight=35.45f;
+		//float num=(float)65.8;
+		//System.out.println("age");
 	}
 
 }
